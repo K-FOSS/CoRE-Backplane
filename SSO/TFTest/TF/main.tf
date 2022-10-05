dbb8e65d664efc04a6c7a4daee960e84242b7e70
kjones <k@kristianjones.dev>
2022-10-05 02:36:28 +0000

    Update SSO/TFTest/TF/main.tf, Apps/SSOTF.yaml

--- SSO/TFTest/TF/main.tf
+++ SSO/TFTest/TF/main.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = ">= 0.12.26"
+
+  required_providers {
+    authentik = {
+      source = "goauthentik/authentik"
+      version = "2022.8.1"
+    }
+  }
 }
 
 variable "aaatoken" {
@@ -8,6 +15,14 @@ variable "aaatoken" {
    description = "Authentik Token"
 }
 
+provider "authentik" {
+  url   = "https://idp.mylogin.space"
+  token = "${var.aaatoken}"
+  # Optionally set insecure to ignore TLS Certificates
+  # insecure = true
+}
+
 output "hello_world" {
   value = "hey hey ya, ${var.aaatoken}!"
 }
+
