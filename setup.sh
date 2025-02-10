9114b898d56b968f1d41f5ac4ac479ec93c23c32
Entrapta Jones <august@mylogin.space>
2025-02-10 15:48:51 +0000

    feat(Development.Che): Work on getting things setup a bit better for a nice and awesome devspace workflow

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "Creating Directory"
+
+mkdir -p ./Meta/bin
+
+
+echo "Getting MQTT CLI"
+
+
+wget -O ./Meta/bin/mqttx https://www.emqx.com/en/downloads/MQTTX/v1.11.1/mqttx-cli-linux-x64
+chmod +x ./Meta/bin/mqttx
+
+
+echo "Getting MC CLI"
+
+wget -O ./Meta/bin/mc https://dl.min.io/client/mc/release/linux-amd64/mc
+chmod +x ./Meta/bin/mc
