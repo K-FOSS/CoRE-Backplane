54df95a139fe12828e8098b985fe9b3cc0082424
Entrapta Jones <august@mylogin.space>
2025-02-18 02:52:32 +0000

    ide(EGCtl): Add egctl to local CLIs and tools

--- setup.sh
+++ setup.sh
@@ -15,3 +15,9 @@ echo "Getting MC CLI"
 
 wget -O ./Meta/bin/mc https://dl.min.io/client/mc/release/linux-amd64/mc
 chmod +x ./Meta/bin/mc
+
+
+echo "Getting egctl"
+
+wget -O - https://github.com/envoyproxy/gateway/releases/download/latest/egctl_latest_linux_amd64.tar.gz | tar -zxf - -C ./Meta/bin/ --strip-components=3
+
