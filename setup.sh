2feedeb00cb1a4b30c97254caa0505fb13088ec6
Entrapta Jones <august@mylogin.space>
2025-02-19 20:22:28 +0000

    ide(Scripts.TalosCTL): Work on getting TalosCTL added to the web IDE

--- setup.sh
+++ setup.sh
@@ -20,4 +20,9 @@ chmod +x ./Meta/bin/mc
 echo "Getting egctl"
 
 wget -O - https://github.com/envoyproxy/gateway/releases/download/latest/egctl_latest_linux_amd64.tar.gz | tar -zxf - -C ./Meta/bin/ --strip-components=3
+chmod +x ./Meta/bin/egctl
 
+echo "Getting talosctl"
+
+wget -O ./Meta/bin/talosctl https://github.com/siderolabs/talos/releases/download/v1.10.0-alpha.1/talosctl-linux-amd64
+chmod +x ./Meta/bin/talosctl
